38b82c3a3615228bb33cf62fd7d6e3a424ae2968
Gesang Paudra J <g.paudra@gmail.com>
2023-09-29 17:38:16 +0700

    Working sorting column

--- src/pages/normalisasi/index.js
+++ src/pages/normalisasi/index.js
@@ -1,8 +1,3 @@
-// Halaman Normalisasi Dataset
-
-// Menampilkan hasil normalisasi sebelumnya
-// Tombol aksi menjalankan normalisasi dengan data terbaru
-
 import React, { useState, useEffect } from 'react'
 import Button from '@mui/material/Button'
 import Alert from '@mui/material/Alert'
@@ -17,9 +12,9 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TablePagination from '@mui/material/TablePagination'
 import Paper from '@mui/material/Paper'
-import SendIcon from '@mui/icons-material/Send'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import SendIcon from '@mui/icons-material/Send'
 
 const columns = [
   { id: 'tanggaljam', label: 'Tanggal Jam', maxWidth: 100, align: 'left', sortable: true },
@@ -27,9 +22,8 @@ const columns = [
   { id: 'pH', label: 'pH', align: 'left', sortable: true },
   { id: 'kelembaban', label: 'Kelembaban', align: 'left', sortable: true },
   { id: 'kondisi', label: 'Kondisi', align: 'left', sortable: false } // No sorting for this column
-]
+];
 
-// Fungsi Sortir kolom
 function createData(tanggaljam, suhu, pH, kelembaban, kondisi) {
   return { tanggaljam, suhu, pH, kelembaban, kondisi };
 }
@@ -46,7 +40,6 @@ function stableSort(array, comparator) {
   return stabilizedThis.map((el) => el[0]);
 }
 
-// Get sorting order (asc or desc)
 function getComparator(order, orderBy) {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
@@ -65,12 +58,12 @@ function descendingComparator(a, b, orderBy) {
 }
 
 const DatasetNormalisasi = () => {
-  const [successMessage, setSuccessMessage] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-  const [data, setData] = useState([])
-  const [page, setPage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(10)
-  const [sorting, setSorting] = useState({ column: 'tanggaljam', direction: 'asc' })
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [sorting, setSorting] = useState({ column: 'tanggaljam', direction: 'asc' });
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -86,29 +79,13 @@ const DatasetNormalisasi = () => {
     setSorting({ column: columnId, direction: isAsc ? 'desc' : 'asc' });
   };
 
-
-
-  /*
-    useEffect(() => {
-      // Fetch data from the server
-      fetch('http://localhost:3001/api/get-normalize-data')
-        .then((response) => response.json())
-        .then((result) => {
-          setData(result);
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-          setErrorMessage('Error fetching data');
-        });
-    }, []);*/
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3001/api/get-normalize-data');
         if (response.ok) {
           const result = await response.json();
-          setData(result); // Update data state
+          setData(result);
         } else {
           console.error('Error Mendapatkan Data Dari Sensor Data');
         }
@@ -116,11 +93,8 @@ const DatasetNormalisasi = () => {
         console.error('Error:', error);
       }
     };
-
     fetchData();
   }, []);
-
-  const sortedData = data;
 
   const handleNormalizationClick = async () => {
     try {
@@ -141,6 +115,9 @@ const DatasetNormalisasi = () => {
     }
   };
 
+  const rows = data.map((row) => createData(row.tanggaljam, row.suhu, row.pH, row.kelembaban, row.kondisi));
+  const sortedData = stableSort(rows, getComparator(sorting.direction, sorting.column));
+
   return (
     <div>
       <Card>
@@ -156,7 +133,8 @@ const DatasetNormalisasi = () => {
             Mulai Normalisasi
           </Button>
         </CardContent>
-      </Card><br></br>
+      </Card>
+      <br></br>
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
         <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
@@ -175,14 +153,15 @@ const DatasetNormalisasi = () => {
                       {column.label}
                       {column.sortable && (
                         <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '4px' }}>
-                          {sorting.column === column.id && (
-                            <>
-                              {sorting.direction === 'asc' ? (
+                          {column.sortable && (
+                            <div style={{ height: '24px' }}>
+                              {sorting.column === column.id && sorting.direction === 'asc' && (
                                 <ArrowUpwardIcon fontSize="small" />
-                              ) : (
+                              )}
+                              {sorting.column === column.id && sorting.direction === 'desc' && (
                                 <ArrowDownwardIcon fontSize="small" />
                               )}
-                            </>
+                            </div>
                           )}
                         </div>
                       )}
@@ -194,27 +173,24 @@ const DatasetNormalisasi = () => {
             <TableBody>
               {sortedData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
                 <TableRow hover role="checkbox" tabIndex={-1} key={row.tanggaljam}>
-                  {columns.map((column) => {
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof row[column.id] === 'number'
-                          ? column.format(row[column.id])
-                          : row[column.id]}
-                      </TableCell>
-                    );
-                  })}
+                  {columns.map((column) => (
+                    <TableCell key={column.id} align={column.align}>
+                      {column.format && typeof row[column.id] === 'number'
+                        ? column.format(row[column.id])
+                        : row[column.id]}
+                    </TableCell>
+                  ))}
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-
         <TablePagination
-          rowsPerPageOptions={[10, 25, 50]}
+          rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={sortedData.length}
-          page={page}
+          count={data.length}
           rowsPerPage={rowsPerPage}
+          page={page}
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
