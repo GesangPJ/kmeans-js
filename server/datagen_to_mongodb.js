2c765a9b9e493134ed842c1285f6cd78c336226b
Gesang Paudra J <g.paudra@gmail.com>
2023-09-29 15:35:27 +0700

    Update data_generator

--- server/datagen_to_mongodb.js
+++ server/datagen_to_mongodb.js
@@ -1,20 +1,21 @@
 // Dummy Data Generator to MongoDB Collection Sensor_Data
+// Data generator untuk membuat data dan memasukkan kedalam MongoDB
 const fs = require('fs');
 const { connectToMongoDB } = require('./mongoDB');
 const { DateTime } = require('luxon');
 
-// Use a dynamic import to load the 'random' module
+// Menggunakan dynamic import untuk 'random' module
 import('random').then(async (randomModule) => {
   const random = randomModule.default;
 
-  // Function to generate a random datetime within a given range
+  // Fungsi generate tanggaljam
   function random_datetime(start_date, end_date) {
     const randomSeconds = random.int(0, Math.floor((end_date - start_date) / 1000));
 
     return start_date.plus({ seconds: randomSeconds });
   }
 
-  // Function to generate random data based on your criteria
+  // Fungsi generate data keseluruhan
   function generate_data() {
     const tanggaljam = random_datetime(DateTime.fromISO('2023-01-01'), DateTime.fromISO('2023-12-31'));
     const suhu = random.int(15, 35);
@@ -31,7 +32,7 @@ import('random').then(async (randomModule) => {
     };
   }
 
-  // Generate data
+  // Mulai generate data
   const data = [];
   for (let i = 0; i < 50; i++) {
     data.push(generate_data());
@@ -40,15 +41,17 @@ import('random').then(async (randomModule) => {
   data.sort((a, b) => a.tanggaljam - b.tanggaljam);
 
   try {
-    // Establish the MongoDB connection
+    // Koneksi ke MongoDB
     const { database } = await connectToMongoDB();
 
-    // Access the 'sensor_data' collection
+    // Set akses ke collection 'sensor_data'
     const collection = database.collection('sensor_data');
 
-    // Insert data into the collection
+    // Hapus data yang sudah ada dalam collection dan ganti dengan yang baru
+    await collection.deleteMany({});
     await collection.insertMany(data);
-    console.log("Data has been inserted into MongoDB.");
+
+    console.log("Data has been replaced in the 'sensor_data' collection.");
   } catch (error) {
     console.error("An error occurred:", error);
   }
