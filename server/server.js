304b3d1189926f29ee3401bf84403a68a13babef
Gesang Paudra J <g.paudra@gmail.com>
2023-09-30 11:57:48 +0700

    rectify normalization process

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const cors = require('cors')
 const multer = require('multer')
 const fetch = require('node-fetch')
 const { fetchNormalizedData, elbowOptimize } = require('./elbow_process')
-const KMeans = require('./kmeans_process');
-
+const KMeans = require('./kmeans_process')
+const { normalizeData } = require('./dataset_normalization') // Import the normalizeData function
 
 const app = express()
 const storage = multer.memoryStorage()
@@ -14,37 +14,6 @@ const upload = multer({ storage })
 // Express JS
 app.use(express.json())
 app.use(cors())
-
-// Method Normalisasi Dataset
-const normalizeData = (data) => {
-  const minMax = {};
-
-  // Sort the data based on tanggaljam in ascending order to get the oldest data first
-  data.sort((a, b) => new Date(a.tanggaljam) - new Date(b.tanggaljam));
-
-  const normalizedData = data.map((record) => {
-    if (!minMax.tanggaljam) {
-      minMax.tanggaljam = { min: record.tanggaljam, max: record.tanggaljam };
-    } else {
-      if (record.tanggaljam < minMax.tanggaljam.min) {
-        minMax.tanggaljam.min = record.tanggaljam;
-      }
-      if (record.tanggaljam > minMax.tanggaljam.max) {
-        minMax.tanggaljam.max = record.tanggaljam;
-      }
-    }
-
-    return {
-      tanggaljam: record.tanggaljam,
-      suhu: (record.suhu - 15) / (35 - 15),
-      pH: record.pH / 14,
-      kelembaban: (record.kelembaban - 30) / (80 - 30),
-      kondisi: (record.kondisi - 1) / (3 - 1),
-    };
-  });
-
-  return { normalizedData, minMax };
-}
 
 // Api Normalisasi Dan Simpan Data
 const normalizeAndSaveData = async () => {
