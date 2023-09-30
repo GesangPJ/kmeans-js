46ef616af395e867e3c1ba9100fc40e1d10de9db
Gesang Paudra J <g.paudra@gmail.com>
2023-09-30 13:10:54 +0700

    change package for kmeans

--- server/kmeansprocess.js
+++ server/kmeansprocess.js
@@ -1,32 +1,42 @@
 // kmeansprocess.js
+// Processing K-Means menggunakan module kmeans-node
 
-const KMeans = require('kmeans-node');
-const math = require('mathjs');
-const { connectToMongoDB } = require('./mongoDB');
+const KMeans = require('ml-kmeans')
+const math = require('mathjs')
+const { connectToMongoDB } = require('./mongoDB')
 
 const calculateMeanCentroid = (data, k) => {
-  const centroid = [];
+  const centroid = []
   for (let i = 0; i < k; i++) {
-    const cluster = data.filter((point) => point.label === i);
+    const cluster = data.filter((point) => point.label === i)
 
-    const clusterMean = cluster.reduce((acc, point) => {
-      return math.add(acc, point.features);
-    }, math.zeros(cluster[0].features.length));
-    centroid.push(math.divide(clusterMean, cluster.length));
+    const clusterMean = {
+      suhu: cluster.reduce((acc, point) => acc + point.suhu, 0) / cluster.length,
+      pH: cluster.reduce((acc, point) => acc + point.pH, 0) / cluster.length,
+      kelembaban: cluster.reduce((acc, point) => acc + point.kelembaban, 0) / cluster.length,
+      kondisi: cluster.reduce((acc, point) => acc + point.kondisi, 0) / cluster.length,
+    }
+
+    centroid.push(clusterMean)
   }
 
-  return centroid;
-};
+  return centroid
+}
 
 const generateRandomCentroid = (data, k) => {
-  const centroid = [];
+  const centroid = []
   for (let i = 0; i < k; i++) {
-    const randomPoint = data[Math.floor(Math.random() * data.length)].features;
-    centroid.push(randomPoint);
+    const randomPoint = data[Math.floor(Math.random() * data.length)]
+    centroid.push({
+      suhu: randomPoint.suhu,
+      pH: randomPoint.pH,
+      kelembaban: randomPoint.kelembaban,
+      kondisi: randomPoint.kondisi,
+    })
   }
 
-  return centroid;
-};
+  return centroid
+}
 
 const calculateKMeans = async (data, k, maxLoop, centroid) => {
   const kmeans = new KMeans({
@@ -35,15 +45,13 @@ const calculateKMeans = async (data, k, maxLoop, centroid) => {
     tolerance: 1e-4,
     runs: 1,
     initialCentroids: centroid,
-  });
+  })
 
-  const result = kmeans.fit(data);
-
-  // After K-Means calculation, you can save the results to MongoDB as you previously did
+  const result = kmeans.fit(data)
 
   // Replace existing data or insert new data in 'kmeans_result' collection
   const { database } = await connectToMongoDB();
-  const kMeansResultCollection = database.collection('kmeans_result');
+  const kMeansResultCollection = database.collection('kmeans_result')
 
   const kMeansResultData = {
     JumlahCluster: k,
@@ -52,9 +60,9 @@ const calculateKMeans = async (data, k, maxLoop, centroid) => {
     Timestamp: new Date(),
   }
 
-  await kMeansResultCollection.replaceOne({}, kMeansResultData, { upsert: true });
+  await kMeansResultCollection.replaceOne({}, kMeansResultData, { upsert: true })
 
   return result; // Return the K-Means results
 }
 
-module.exports = { calculateKMeans, calculateMeanCentroid, generateRandomCentroid };
+module.exports = { calculateKMeans, calculateMeanCentroid, generateRandomCentroid }
